ab30f489250d46820e531d6c7a92e7c7e49d87bd
Dominik Lechl <dominik.lechl@alturos.com>
2023-03-21 14:07:49 +0100

    Setup material theming (allowing night mode too) and format whole project according to android studio default formatting

--- app/src/main/java/at/aau/edu/wizards/MainActivity.kt
+++ app/src/main/java/at/aau/edu/wizards/MainActivity.kt
@@ -1,7 +1,7 @@
 package at.aau.edu.wizards
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
