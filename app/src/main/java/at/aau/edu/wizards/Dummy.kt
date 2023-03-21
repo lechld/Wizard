085f915d4eafefbef38ccd1a2775b1c162783d97
Dominik Lechl <dominik.lechl@alturos.com>
2023-03-21 13:25:35 +0100

    Add some dummy test code

---
+++ app/src/main/java/at/aau/edu/wizards/Dummy.kt
@@ -0,0 +1,10 @@
+package at.aau.edu.wizards
+
+class Dummy {
+
+    var counter = 0
+
+    fun increase() {
+        counter++
+    }
+}
---
+++ app/src/test/java/at/aau/edu/wizards/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package at.aau.edu.wizards
+
+import org.junit.jupiter.api.Test
+
+class MainActivityTest {
+
+    @Test
+    fun someTest() {
+        assert(true)
+    }
+}
