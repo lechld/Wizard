11c2c8df35ccba5bf6bd0e38223e13c64423dd1f
Dominik Lechl <dominik.lechl@alturos.com>
2023-03-28 13:39:45 +0200

    feat(networking): provide basic permission requests

--- app/src/main/java/at/aau/edu/wizards/MainActivity.kt
+++ app/src/main/java/at/aau/edu/wizards/MainActivity.kt
@@ -1,11 +1,22 @@
 package at.aau.edu.wizards
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import at.aau.edu.wizards.sample.SampleFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val permissions = listOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.BLUETOOTH_ADVERTISE,
+        android.Manifest.permission.BLUETOOTH_CONNECT,
+        android.Manifest.permission.BLUETOOTH_SCAN,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +26,25 @@ class MainActivity : AppCompatActivity() {
                 showSampleFragment()
             }
         }
+
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            println(permissions)
+        }
+
+
+        val ungranted = permissions.mapNotNull { requiredPermission ->
+            val requestResult = ContextCompat.checkSelfPermission(this, requiredPermission)
+
+            if (requestResult == PackageManager.PERMISSION_GRANTED) {
+                return@mapNotNull null
+            }
+
+            return@mapNotNull requiredPermission
+        }
+
+        permissionLauncher.launch(ungranted.toTypedArray())
     }
 
     private fun showSampleFragment() {
