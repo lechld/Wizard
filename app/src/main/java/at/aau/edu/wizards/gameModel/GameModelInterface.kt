1fb3875f7fad11f71ca670fc3f5f5a5446a84113
Kirschzelle <nikolassc@edu.aau.at>
2023-04-04 12:10:04 +0200

    fix(logic): expanded function description for GameModel with the legal parameters that can be used to play the game.

--- app/src/main/java/at/aau/edu/wizards/gameModel/GameModelInterface.kt
+++ app/src/main/java/at/aau/edu/wizards/gameModel/GameModelInterface.kt
@@ -14,6 +14,13 @@ interface GameModelInterface {
     /**
      * Receives a move from the server, and updates the internal Board depending on the received move.
      * Returns a [GameModelResult] depending on if it was successful or not.
+     *
+     * Legal Moves:
+     * You can send a card, this represents you playing the card: It consists of a three letter string consisting of the value, color and player id. Value between 0 and 14, color between 1 and 3 and player between 0 and 2-5 (depending on amount of players).
+     * You can send a guess, this is represented by a value starting at ten for guess 0 for player 0. It then goes up module 11 since you cant guess more than 10.
+     * So starting at 21 you would guess 0 for player 1, 32 for player 2, etc. etc.
+     * You can send a trump, this is represented by a normal card, but player id is 6. (Reserved for System cards.) Card 0 0 6 is also accepted, this means there is no trump.
+     * Just 1 is used to update the data listener, this only used for internal updates after a new configuration for example and should not be called. (Though it wont result in any error, it is just useless.)
      */
     fun receiveMove(move: String): GameModelResult<Unit>
 
