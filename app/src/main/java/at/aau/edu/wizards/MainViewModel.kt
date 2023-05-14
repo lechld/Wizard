82a4d42af0ae9e979a00b3e873a940107b4736d2
ena <enkecanovic@edu.aau.at>
2023-05-14 21:56:15 +0200

    Create MainViewModel.kt

---
+++ app/src/main/java/at/aau/edu/wizards/MainViewModel.kt
@@ -0,0 +1,25 @@
+package at.aau.edu.wizards
+
+import android.content.SharedPreferences
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModel constructor(private val sharedPreferences: SharedPreferences) : ViewModel() {
+
+    fun getUsername(): String? {
+        return sharedPreferences.getString(SHARED_PREFERENCE_USERNAME_KEY, null)
+    }
+
+    fun saveUsername(username: String) {
+        sharedPreferences.edit().putString(SHARED_PREFERENCE_USERNAME_KEY, username).apply()
+    }
+
+    class Factory(private val sharedPreferences: SharedPreferences) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                MainViewModel(sharedPreferences) as T
+            } else throw IllegalStateException("Wrong Factory for instantiating ${modelClass::class.java.canonicalName}")
+        }
+    }
+}
