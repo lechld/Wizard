e0cd29e0d61f5d6e5fb4852a96aeb2650797502c
ena <enkecanovic@edu.aau.at>
2023-05-14 22:33:59 +0200

    Fix smell: remove unused import

--- app/src/main/java/at/aau/edu/wizards/MainActivity.kt
+++ app/src/main/java/at/aau/edu/wizards/MainActivity.kt
@@ -1,7 +1,6 @@
 package at.aau.edu.wizards
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
