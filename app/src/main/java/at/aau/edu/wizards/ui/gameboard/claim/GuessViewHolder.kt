4c1d97ad544a2d2b22da4fbe6ba8d91bfc2a7aa5
Ena Kecanovic <enkecanovic@edu.aau.at>
2023-05-17 19:09:45 +0200

    Feat(ui):feature/haptic_feedback
    Added haptic feedback when guess is made. Please review if possible!

--- app/src/main/java/at/aau/edu/wizards/ui/gameboard/claim/GuessViewHolder.kt
+++ app/src/main/java/at/aau/edu/wizards/ui/gameboard/claim/GuessViewHolder.kt
@@ -1,17 +1,34 @@
 package at.aau.edu.wizards.ui.gameboard.claim
 
+import android.content.Context
+import android.os.VibrationEffect
+import android.os.Vibrator
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import at.aau.edu.wizards.databinding.ItemGuessBinding
 
 class GuessViewHolder(
     private val binding: ItemGuessBinding,
     private val onClick: (Int) -> Unit,
+    private val context: Context
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(value: Int) {
         binding.text.text = value.toString()
         binding.root.setOnClickListener {
             onClick(value)
+
+            // Trigger haptic feedback
+            val vibrator = ContextCompat.getSystemService(context, Vibrator::class.java)
+            if (vibrator?.hasVibrator() == true) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+                } else {
+                    // Deprecated in API 26
+                    @Suppress("DEPRECATION")
+                    vibrator.vibrate(50)
+                }
+            }
         }
     }
 }
