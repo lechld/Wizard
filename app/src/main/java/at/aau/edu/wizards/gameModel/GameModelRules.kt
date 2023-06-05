40e010aeb43d153d117e08a644003ed93609e1ba
Kirschzelle <nikolassc@edu.aau.at>
2023-06-05 09:33:51 +0200

    fix(logic): Fixing bug where game would never update on cpu play.

--- app/src/main/java/at/aau/edu/wizards/gameModel/GameModelRules.kt
+++ app/src/main/java/at/aau/edu/wizards/gameModel/GameModelRules.kt
@@ -199,6 +199,7 @@ class GameModelRules(
             delay(1000)
             nextSet()
         } else if (!players[currentPlayer].isHuman) {
+            parent.listener.update()
             getCpuToPlay()
         }
     }
@@ -210,6 +211,7 @@ class GameModelRules(
         if (players[0].cards.isEmpty()) {
             nextRound()
         } else if (!players[currentPlayer].isHuman) {
+            parent.listener.update()
             getCpuToPlay()
         }
     }
@@ -249,6 +251,7 @@ class GameModelRules(
             wantsGuess = false
         }
         if (everyoneHasGuessed() && !players[currentPlayer].isHuman) {
+            parent.listener.update()
             getCpuToPlay()
         }
     }
@@ -263,7 +266,6 @@ class GameModelRules(
 
     private suspend fun getCpuToPlay() {
         delay(1000)
-
         parent.receiveMessage(cpu.getMove(currentPlayer).getString())
     }
 }
